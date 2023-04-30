8afc63add045f4271d06ab51d1cd50f6b72d4ba1
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-04-30 00:45:53 -0400

    Setting up Config Files
    Added
    1. mongoCollection.js -> DB collection Initialization
    2. mongoConnection.js -> DB connection
    3. settings.js -> DB configuration
    
    All these are done following the format thought by Professor Patrick Hill from CS546 class lecture code.

---
+++ config/mongoCollections.js
@@ -0,0 +1,23 @@
+import {dbConnection} from './mongoConnection.js';
+
+/* This will allow you to have one reference to each collection per app */
+/* Feel free to copy and paste this this */
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+/* Now, you can list your collections here: 
+NOTE: YOU WILL NEED TO CHANGE THE CODE BELOW AND UNCOMMENT IT TO HAVE THE COLLECTION(S) REQUIRED BY THE ASSIGNMENT */
+
+//export const users = getCollectionFn('users');
+export const primaryUsers = getCollectionFn('primaryUsers');
+export const scoutUsers = getCollectionFn('scoutUsers');
---
+++ config/settings.js
@@ -0,0 +1,6 @@
+//YOU WILL NEED TO CHANGE THE DB NAME TO MATCH THE REQUIRED DB NAME IN THE ASSIGNMENT SPECS!!!
+
+export const mongoConfig = {
+  serverUrl: 'mongodb://localhost:27017/',
+  database: 'Apartment_Scouting_DB'
+};
