8183d30d6c3f64e47946218ba967ad83c16aed6c
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-05-01 15:57:42 -0400

    fixed bug in settings.js for windows systems
    Fixed bug where mongoDB setup doesn't work for windows

--- config/settings.js
+++ config/settings.js
@@ -4,3 +4,5 @@ export const mongoConfig = {
   serverUrl: 'mongodb://localhost:27017/',
   database: 'Apartment_Scouting_DB'
 };
+
+//If you are a windows user change the serverURL to 'mongodb://127.0.0.1:27017
